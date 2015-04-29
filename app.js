cba2fc4bbd4275c864b8661a945a46a8e72406ae
Larry Boniface <Lawrence.Boniface@gmail.com>
2015-04-28 21:45:22 -0600

    v2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	var color = 'white';
 	var colors = 'red blue green yellow white';
+	
 	$('.box').on('click', function() {
 		$(this).addClass(color);
 	})
@@ -8,21 +9,27 @@ $(document).ready(function() {
 		$(this).removeClass(color);
 	})
 	$('#reset').on('click', function() {
-		$('.box').removeClass(colors);
+		$('.box').removeClass(colors),
+		alert('Colors Reset!');
 	})
 	$('#red').on('click', function() {
-		color = 'red';
+		color = 'red',
+		alert("Color set to to " + color +"!");
 	})
 	$('#green').on('click', function() {
-		color = 'green';
+		color = 'green',
+		alert("Color set to to " + color +"!");
 	})
 	$('#blue').on('click', function() {
-		color = 'blue';
+		color = 'blue',
+		alert("Color set to to " + color +"!");
 	})
 	$('#yellow').on('click', function() {
-		color = 'yellow';
+		color = 'yellow',
+		alert("Color set to to " + color +"!");
 	})
 	$('#white').on('click', function() {
-		color = 'white';
+		color = 'white',
+		alert("Color set to to " + color +"!");
 	})
 });
